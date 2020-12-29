f2233317bdb4d3ae769cc4103626e0621c27c5c0
ASSASSINYM <darlingiloveu@sina.com>
2020-12-29 11:44:01 +0800

    Initial Commit

---
+++ iOSIBDesignableDemo/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  iOSIBDesignableDemo
+//
+//  Created by ASSASSIN on 2020/12/29.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
